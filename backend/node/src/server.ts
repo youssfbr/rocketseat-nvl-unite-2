69ba4f5b823ed04e2b15a30087721e3f3c0f75a3
Alisson Youssf <youssfbr@gmail.com>
2024-04-03 23:24:48 -0300

    Event POST validation

--- backend/node/src/server.ts
+++ backend/node/src/server.ts
@@ -1,9 +1,35 @@
 import fastify from "fastify";
+import { z } from 'zod';
+import { PrismaClient } from '@prisma/client';
 
 const app = fastify();
 
-app.get('/', () => { return 'Hello World Unite' })
-app.get('/teste', () => { return 'Hello World Teste' })
+const prisma = new PrismaClient({
+  log: ['query'],  
+})
+
+app.post('/events', async (request, reply) => { 
+  const createEventSchema = z.object({
+    title: z.string().min(4),
+    details: z.string().nullable(),
+    maximumAttendees: z.number().int().positive().nullable()
+  })
+
+  const data = createEventSchema.parse(request.body)
+
+  const event = await prisma.event.create({
+    data: {
+      title: data.title,
+      details: data.details,
+      slug: new Date().toISOString(),
+      maximumAttendees: data.maximumAttendees
+    },
+  });
+
+  // return { eventId: event.id };
+  return reply.status(201).send({ eventId: event.id });
+})
+
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running!');
